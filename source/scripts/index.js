3c09865f6b332264043089b51bfbf780d25399e4
KristinaVaniukova <kristinavaniukova@gmail.com>
2024-05-31 16:04:50 +0300

    Добавление исправлений

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
 /* в этот файл добавляет скрипты*/
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.remove('pagebody--nojs');
+
+  const navMain = document.querySelector('.main-nav');
+  if(navMain) {
+    const navToggle = navMain.querySelector('.main-nav__toggle');
+    const navWrapper = navMain.querySelector('.main-nav__wrapper');
+    navToggle.addEventListener('click', () => navWrapper.classList.toggle('main-nav__wrapper--open'));
+  }
+});
