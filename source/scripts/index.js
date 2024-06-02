bdc804f70b3be72070afb0b36e813697d3109525
KristinaVaniukova <kristinavaniukova@gmail.com>
2024-06-02 13:34:04 +0300

    Удаление ненужных файлов, изменение переменных, исправленное бургер-меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const navMain = document.querySelector('.main-nav');
   if(navMain) {
     const navToggle = navMain.querySelector('.main-nav__toggle');
-    const navWrapper = navMain.querySelector('.main-nav__wrapper');
-    navToggle.addEventListener('click', () => navWrapper.classList.toggle('main-nav__wrapper--open'));
+   // const navWrapper = navMain.querySelector('.main-nav__wrapper');
+    navToggle.addEventListener('click', () => navMain.classList.toggle('main-nav--open'));
   }
 });
