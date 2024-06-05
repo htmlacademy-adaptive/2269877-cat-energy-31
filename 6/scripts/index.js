5fde8437c3728cd9baec8f73d380b48a53c964f1
KristinaVaniukova <KristinaVaniukova@users.noreply.github.com>
2024-06-05 13:51:19 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2269877-cat-energy-31@ba3a76e3f8dfeac40c299c900d9fdaf072d04b7a 🚀

---
+++ 6/scripts/index.js
@@ -0,0 +1,1 @@
+window.addEventListener("DOMContentLoaded",()=>{document.body.classList.remove("page__body--nojs");let e=document.querySelector(".main-nav");e&&e.querySelector(".main-nav__toggle").addEventListener("click",()=>e.classList.toggle("main-nav--open"))});
